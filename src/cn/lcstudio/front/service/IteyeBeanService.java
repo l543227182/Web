5375d393f18bb6ed23c11bb6332435d8944af518
LC <543227182@qq.com>
2017-10-31 14:25:44 +0800

    add-

--- src/cn/lcstudio/front/service/IteyeBeanService.java
+++ src/cn/lcstudio/front/service/IteyeBeanService.java
@@ -6,9 +6,15 @@ import cn.lcstudio.bean.IteyeBean;
 import java.util.List;
 
 public interface IteyeBeanService {
-	public int addBean(IteyeBean bean);
-	public void delBean(String[] id);
-	public IteyeBean getBeanByID(String id);
-	public Pagination getListBean(Integer pageNo,boolean back,Integer pageSize);
-	public String[] getAllID();
+    public int addBean(IteyeBean bean);
+
+    public void delBean(String[] id);
+
+    public IteyeBean getBeanByID(String id);
+
+    public Pagination getListBean(Integer pageNo, boolean back, Integer pageSize);
+
+    public String[] getAllID();
+
+    public List<IteyeBean> getAll();
 }
